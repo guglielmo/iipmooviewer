2f54df015a03a8b81f876693301d708358924063
Guglielmo <guglielmo.celata@gmail.com>
2011-01-18 17:03:23 +0100

    aggiunta gestione eccezione per incunabili

--- page_turner_json.php
+++ page_turner_json.php
@@ -3,12 +3,28 @@ $iip_viewer_host = 'http://iipimageviewer/viewimg.php';
 $image_path = $_POST["path"];
 $image_name = str_replace('.', ':', $_POST["name"]);
 $collection = $_POST["collection"];
-$page_turner_service_url = "http://abiblita/frontend_dev.php/page_turner/$image_name/$collection";
+
+# gestisce eccezione incunaboli
+if ($collection == 'incunaboli') {
+  $path_ar = explode("/", $image_path);
+  $n = count($path_ar) - 2;
+  $prefix = $path_ar[$n];
+  $image_name = sprintf("%s_%s", $prefix, $image_name);
+}
+
+$page_turner_service_url = "http://abiblita/page_turner/$image_name/$collection";
 $xml_obj = simplexml_load_file($page_turner_service_url);
 
 $next_page = (string)$xml_obj->next_page;
 $prev_page = (string)$xml_obj->prev_page;
 $current_page = (string)$xml_obj->current_page;
+
+# per incunaboli, strip della stringa
+if ($collection == 'incunaboli') {
+  $l = strlen("file:///Scan");
+  $next_page = substr($next_page, $l);
+  $prev_page = substr($prev_page, $l);
+}
 
 $pages_urls = array(
   'current_page'  => "$iip_viewer_host?img_path=$image_path&img_name=$current_page",
